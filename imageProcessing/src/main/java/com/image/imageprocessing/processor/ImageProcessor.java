013a693828d54dcd4ab90c23615265da25b724d4
aditya khandade <adityakhandade2002@gmail.com>
2025-08-28 17:44:48 +0530

    Added imageProcessor

--- imageProcessing/src/main/java/com/image/imageprocessing/processor/ImageProcessor.java
+++ imageProcessing/src/main/java/com/image/imageprocessing/processor/ImageProcessor.java
@@ -2,6 +2,7 @@ package com.image.imageprocessing.processor;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageFilter;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,6 +20,22 @@ public class ImageProcessor {
     }
 
     public void processImage(BufferedImage image, int num, ImageFilter imageFilter) {
+        int numHorizontalImages = image.getWidth() / num;
+        int numVerticalImages = image.getWidth() / num;
+
+        for (int i = 0; i < numVerticalImages; i++) {
+            for (int j = 0; j < numHorizontalImages; j++) {
+                BufferedImage subImage = image.getSubimage(i * num, j * num, num, num);
+                executorService.submit(new Callable<BufferedImage>() {
+                    @Override
+                    public BufferedImage call() {
+                        // return null because we use callable and in it we have to return something
+                        // runnable means returns nothing
+                        return imageFilter.filter(subImage);
+                    }
+                });
+            }
+        }
 
     }
 }
